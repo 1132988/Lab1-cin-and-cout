3120c470045e7b3a1b23c7c776b1d247b5076b45
t13rm4in <119757490+t13rm4in@users.noreply.github.com>
2022-12-29 19:28:20 +0300

    Добавьте файлы проекта.

---
+++ ЛР 1 (ввод и вывод данных)/ЛР 1 (ввод и вывод данных).cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float a, b, c;
+	cout << "Введите a: ";
+	cin >> a;
+	cout << "Введите b: ";
+	cin >> b;
+	c = sqrt(a * b);
+	cout <<"Среднее геометрическое a и b = "<<c;
+}
